9932dfeb931c62b49b44214501d66e1116c37e30
vinivin153 <vinivin153@naver.com>
2025-01-18 23:29:06 +0900

    feat: baseURL 변경

--- app/src/main/java/com/twomuchcar/usedcar/service/ApiClient.kt
+++ app/src/main/java/com/twomuchcar/usedcar/service/ApiClient.kt
@@ -7,26 +7,16 @@ import okhttp3.logging.HttpLoggingInterceptor
 
 object ApiClient {
     //    private const val BASE_URL = "http://10.0.2.2:8080"
-    private const val BASE_URL = "https://twomuchcar.shop"
+//    private const val BASE_URL = "https://twomuchcar.shop"
+    private const val BASE_URL = "https://autoever.site"
 
     val api: ApiService by lazy {
         val logging = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY // 요청/응답 로그 활성화
         }
-        // AccessToken은 현재 로그인한 유저의 토큰으로 넣어줘야 합니다.
-        val client = OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                val request = chain.request().newBuilder()
-                    .addHeader("Cookie", "AccessToken=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Imtha2FvIDM4NzkyMTkwMDYiLCJyb2xlIjoiUk9MRV9VU0VSIiwiZW1haWwiOiJqYW5naGU5QG5hdmVyLmNvbSIsImlhdCI6MTczNzAzNDAyMywiZXhwIjoxNzM3MTIwNDIzfQ.-JajgtoflauPvj8xhSD0op5OX7Ai-leXO5hF_U0XXzc")
-                    .build()
-                chain.proceed(request)
-            }
-            .addInterceptor(logging)
-            .build()
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
